a4bccc74a70988ca27dd05da76cf6174a9207461
paanhutmockupwebsite <paanhutmockupwebsite@gmail.com>
2026-05-31 10:46:40 -0400

    update

--- components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "../src/styles/Footer.css"
-
-function Footer(){
-    return(
-        <p className="footerText">Copyright © 2026 Paan Hut. All Rights Reserved.</p>
-    )
-}
-
-export default Footer
